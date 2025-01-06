1f2175bed545515020636a17ea9fc3d0099effe3
Brodzman <thbrodz@gmail.com>
2025-01-07 03:13:11 +1100

    Add players turn
    Allowed the player to input a move that will keep prompting until they input a valid move

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice >= 0 && computerChoice < 0.33) {
         return 'rock';
-    } else if (computerChoice >= 0.33 && computerChoice < 0.66) {
+    } else if (computerChoice >= 0.33
+        && computerChoice < 0.66) {
         return 'paper';
     } else {
         return 'scissors';
@@ -12,4 +13,28 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(humanChoice) {
+    if (humanChoice == 'rock'
+        || humanChoice == 'paper'
+        || humanChoice == 'scissors') {
+            return humanChoice;
+        } else {
+            return null;
+        }
+}
+
+
+
+function playerTurn() {
+    let userInput = prompt();
+    let output = getHumanChoice(userInput);
+    if (output === null) {
+        playerTurn();
+    } else
+    console.log(output);
+}
+
+
+playerTurn();
+
+
